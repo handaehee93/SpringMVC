3daf6b3593de46f3fd58928578ffaa76d0ab9db0
handaehee93 <hdh8990@naver.com>
2024-04-01 22:10:24 +0900

    multipart로 보낸 데이터 확인

---
+++ fileupload/src/main/java/mvc/fileupload/controller/ServletUploadControllerV1.java
@@ -0,0 +1,43 @@
+package mvc.fileupload.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v1")
+public class ServletUploadControllerV1 {
+
+    // upload-form으로 이동
+    @GetMapping("/upload")
+    public String newFile() {
+        return "upload-form";
+    }
+
+    // post요청 처리하는 메서드
+    @PostMapping("/upload")
+    public String saveFileV1(HttpServletRequest request) throws
+            ServletException, IOException {
+        // request에 어떤 것이 들어오는지 출력
+        log.info("request={}", request);
+
+        // 상품명 input 태그의 값을 출력
+        String itemName = request.getParameter("itemName");
+        log.info("itemName={}", itemName);
+
+        // multipart로 보내진 각각으 파트들을 출력
+        Collection<Part> parts = request.getParts();
+        log.info("parts={}", parts);
+
+        return "upload-form";
+    }
+}
